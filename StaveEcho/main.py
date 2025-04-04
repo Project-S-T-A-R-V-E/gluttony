2aa3febb163dcb242b2c1c2e70d202bae99a3a4d
EthanJoyner <el2joyner.com>
2025-04-04 03:35:15 +0000

    rm gps in py call

--- StaveEcho/main.py
+++ StaveEcho/main.py
@@ -38,7 +38,7 @@ def handleMsg(msg):
                  imu.getPYR()[0], imu.getPYR()[1], imu.getPYR()[2], 
                  imu.getAccelXYZ()[0], imu.getAccelXYZ()[1], imu.getAccelXYZ()[2], 
                  imu.getMagXYZ()[0], imu.getMagXYZ()[1], imu.getMagXYZ()[2],
-                 gps.getGPS()[0],gps.getGPS()[1],
+                #  gps.getGPS()[0],gps.getGPS()[1],
                  rangeFinder.getRangeFinder()])
     # print ("Message Received: " + msg)
 
