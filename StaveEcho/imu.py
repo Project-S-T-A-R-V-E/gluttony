ca555c933db4c4472b9801e0a0cffd4e014d3be3
Ethan Joyner <el2joyner@gmail.com>
2025-04-08 13:03:33 -0400

    fix: update getIMU function to return only gyro data and handle sensor access errors

--- StaveEcho/imu.py
+++ StaveEcho/imu.py
@@ -10,7 +10,8 @@ def getIMU(): #TODO: Implement actual sensor reading
         pyr = sensor.read_gyro_data()
         accel = sensor.read_accel_data()
         mag = sensor.read_mag_data()
-        return (pyr['x'], pyr['y'], pyr['z'], accel['x'], accel['y'], accel['z'], mag['x'], mag['y'], mag['z'])
+        return pyr
+        # return (pyr['x'], pyr['y'], pyr['z'], accel['x'], accel['y'], accel['z'], mag['x'], mag['y'], mag['z'])
     except OSError as e:
         print(f"Error accessing the sensor: {e}")
         return None
