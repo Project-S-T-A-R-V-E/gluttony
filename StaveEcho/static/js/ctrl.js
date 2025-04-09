52d5e9bd8a84c9af622154e6f4eacda4739905b3
Ethan Joyner <el2joyner@gmail.com>
2025-04-09 19:59:14 -0400

    fix: update socket connection URL in ctrl.js

--- StaveEcho/static/js/ctrl.js
+++ StaveEcho/static/js/ctrl.js
@@ -177,7 +177,7 @@ function getDigital(dSignal){
 // Socket.io Variables
 // import { gamepads } from "./ctrl.js";
     const serverIp = "{{ server_ip }}";
-    const socket = io(`http://127.0.0.1:5000`);
+    const socket = io(`https://fond-jointly-oryx.ngrok-free.app/`);
 socket.on('connect',function(){
     // A sort of hand shake to tell the server to send Data 
     // socket.send('ctrl');
