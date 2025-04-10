7ab7c20cad00fbda8829f1e0262a17ae7b2f98b0
Ethan Joyner <el2joyner@gmail.com>
2025-04-10 13:12:20 -0400

    fix: update actuation function to use baseDeg for servo angle

--- StaveEcho/motorControl.py
+++ StaveEcho/motorControl.py
@@ -17,8 +17,8 @@ def actuation(pitchDeg, yawDeg, armHeight, baseDeg):
     yaw = 2
     pitch = 3
     linact = 4
-    print(type(baseDeg))
-    move.servo[base].angle = 90
+    # print(type(baseDeg))
+    move.servo[base].angle = baseDeg
     # move.servo[baseRef].angle = (-1*baseDeg) +180
     # move.servo[yaw].angle = yawDeg    
     # move.servo[pitch].angle = pitchDeg
