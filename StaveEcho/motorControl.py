55bf6f89569264f2320de87a7f27dd41a274b0ad
Ethan Joyner <el2joyner@gmail.com>
2025-04-10 13:15:40 -0400

    fix: cast baseDeg to int for servo angle assignment

--- StaveEcho/motorControl.py
+++ StaveEcho/motorControl.py
@@ -18,7 +18,7 @@ def actuation(pitchDeg, yawDeg, armHeight, baseDeg):
     pitch = 3
     linact = 4
     # print(type(baseDeg))
-    move.servo[base].angle = baseDeg
+    move.servo[base].angle = int(baseDeg)
     # move.servo[baseRef].angle = (-1*baseDeg) +180
     # move.servo[yaw].angle = yawDeg    
     # move.servo[pitch].angle = pitchDeg
